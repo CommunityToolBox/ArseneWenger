ddadba89d85cb596b39153feb9dd2eca46cf4986
RrezonB <51490564+RrezonB@users.noreply.github.com>
2021-04-23 13:31:16 +0200

    Update last5.py
    Used formatting to make it shorter and more readable

--- last5.py
+++ last5.py
@@ -4,27 +4,21 @@ import requests
 from bs4 import BeautifulSoup
 import lxml
 client = discord.Client()
+bob =123
 class GameCheck:
     def __init__(self, name):
-        if name == "leicester":
-            source = requests.get("https://www.11v11.com/teams/arsenal/tab/opposingTeams/opposition/Leicester%20City/")
-        elif name == "manutd":
-            source = requests.get("https://www.11v11.com/teams/arsenal/tab/opposingTeams/opposition/Manchester%20United/")
-        elif name == "mancity":
-            source = requests.get("https://www.11v11.com/teams/arsenal/tab/opposingTeams/opposition/Manchester%20City/")
+            source = requests.get(f"https://www.11v11.com/teams/arsenal/tab/opposingTeams/opposition/{name}")
+            soup = BeautifulSoup(source.text, 'lxml')
+            games = soup.find_all(class_="result-status")
+            allgames = []
 
-
-        soup = BeautifulSoup(source.text, 'lxml')
-        games = soup.find_all(class_="result-status")
-        allgames = []
-
-        for game in games:
-            allgames.append(game.text)
-        self.last5 = allgames[-5:]
+            for game in games:
+                allgames.append(game.text)
+            self.last5 = allgames[-5:]
         
-oqr = GameCheck("leicester")
-utd = CameCheck("manutd")
-mcy = GameCheck("mancity")
+oqr = GameCheck("Leicester City/")
+utd = GameCheck("Manchester United/")
+mcy = GameCheck("Manchester City/")
 
 @client.event
 async def on_ready():
@@ -35,12 +29,9 @@ async def on_message(message):
     if message.author == client.user:
         return
     if message.content.startswith('!history leicester'):
-        await message.channel.send("Our last 5 games with this club:")
-        await message.channel.send(oqr.last5)
+        await message.channel.send(f"`Our last 5 games with this club: {oqr.last5}`")
     elif message.content.startswith('!history manutd'):
-        await message.channel.send("Our last 5 games with this club:")
-        await message.channel.send(utd.last5)
+        await message.channel.send(f"`Our last 5 games with this club: {utd.last5}`")
     elif message.content.startswith('!history mancity'):
-        await message.channel.send("Our last 5 games with this club:")
-        await message.channel.send(mcy.last5)
+       await message.channel.send(f"`Our last 5 games with this club: {mcy.last5}`")
 client.run("token")
