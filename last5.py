1042a627a8ab0ab339d6a88442c62e9ac8f6651f
RrezonB <51490564+RrezonB@users.noreply.github.com>
2021-04-25 12:37:05 +0200

    Update last5.py

--- last5.py
+++ last5.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 import lxml
 client = discord.Client()
 
-class GameCheck:
+class Gamecheck:
     def __init__(self, name):
             source = requests.get(f"https://www.11v11.com/teams/arsenal/tab/opposingTeams/opposition/{name}")
             soup = BeautifulSoup(source.text, 'lxml')
@@ -15,19 +15,14 @@ class GameCheck:
             for game in games:
                 allgames.append(game.text)
             self.last5 = allgames[-5:]
-      
-oqr = GameCheck("Leicester City/")
-utd = GameCheck("Manchester United/")
-mcy = GameCheck("Manchester City/")
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content.startswith('!history leicester'):
-        await message.channel.send(f"`Our last 5 games with this club: {oqr.last5}`")
-    elif message.content.startswith('!history manutd'):
-        await message.channel.send(f"`Our last 5 games with this club: {utd.last5}`")
-    elif message.content.startswith('!history mancity'):
-       await message.channel.send(f"`Our last 5 games with this club: {mcy.last5}`")
+    if message.content.startswith('!history'):
+      team = " ".join(message.content.split()[1:]) #The team name has to be the full team name example Manchester United, with a space or it does not work
+      qor = Gamecheck(team)
+      print(Gamecheck(team))
+      await message.channel.send(f"`Our last 5 games with this club: {qor.last5}`")
 client.run("token")
