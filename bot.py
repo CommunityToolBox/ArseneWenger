2848f8c7e0e46eaf28f858be68f36faa3089f9ed
vaanir <vaanir@outlook.com>
2023-04-18 21:14:04 +0100

    Simplify bot.py for DiscordPy 2.2.2

--- bot.py
+++ bot.py
@@ -1,57 +1,38 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 import os
-import asyncio
-from time import sleep
 
 import discord
 from discord.ext import commands
-
-from utils import getTimestamp
 
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
-prefix = '!'
-bot = commands.Bot(
-    command_prefix=prefix,
-    case_insensitive=True,
-    intents=intents
-)
+class Bot(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix="", intents=intents)
 
+    async def startup(self):
+        await bot.wait_until_ready()
+        await bot.tree.sync()
+        print('Successfully synced applications commands')
+        print(f'Connected as {bot.user}')
 
-async def load_cogs() -> None:
-    # Set cogs that require loading in a specific order
-    # They will be loaded in the order of the list, followed by all
-    # other cogs in the /cogs folder
-
-    if os.path.exists("./cogs"):
-        for file in os.listdir("./cogs"):
-            if file.endswith(".py"):
-                cog_name = file[:-3]
+    async def setup_hook(self):
+        for filename in os.listdir("./cogs"):
+            if filename.endswith(".py"):
                 try:
-                    await bot.load_extension(f'cogs.{cog_name}')
-                    print(f'Loaded cog: {cog_name}')
-                except commands.errors.ExtensionNotFound:
-                    print(f'Failed to load cog: {cog_name}')
+                    await bot.load_extension(f"cogs.{filename[:-3]}")
+                    print(f"Loaded {filename}")
+                except Exception as e:
+                    print(f"Failed to load {filename}")
+                    print(f"[ERROR] {e}")
 
-
-@bot.event
-async def on_ready():
-    await bot.change_presence(activity=discord.Game(name='Wengerball'))
-    print('Logged on as')
-    print(bot.user.name)
-    print('----')
-
+        self.loop.create_task(self.startup())
 try:
     f = open('token.txt')
     token = f.readline()
     f.close()
+    bot = Bot()
+    bot.run(token)
 except Exception as e:
-    print(getTimestamp() +"setup error\n")
     print(e)
-    sleep(5)
-
-asyncio.run(load_cogs())
-bot.run(token)
